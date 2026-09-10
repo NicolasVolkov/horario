f66dba5d7b5c3b8539ded237eef70e8fdc091aa0
NicolasVolkov <kroterup@gmail.com>
2026-09-10 16:35:09 -0300

    Prova Parte 3
    Código principal da prova da pasta Principall para separação

--- Principal.java
+++ Principal.java
@@ -1,28 +1,29 @@
-package Coonta.Princ;
+package Prova.Principall;
 
-import Coonta.TipoConta2.ContaPoupanca;
+import Prova.livros.Livro;
+import Prova.livros.LivroDigital;
+import Prova.livros.LivroFisico;
+import Prova.usuarios.Biblioteca;
+import Prova.usuarios.Usuario;
+
 public class Principal {
     public static void main(String[] args) {
-        ContaPoupanca conta = new ContaPoupanca("João da Silva", "12345-6", 1000.00, "Banco do Brasil", 0.5);
-        System.out.println("--- DADOS DA CONTA ---");
-        conta.mostrarDados();
-
-        System.out.println("\n--- DEPÓSITO ---");
-        conta.depositar(500.00);
-        System.out.println("Saldo após depósito: R$ "
-                + conta.getSaldo());
-
-        System.out.println("\n--- SAQUE ---");
-        conta.sacar(200.00);
-        System.out.println("Saldo após saque: R$ "
-                + conta.getSaldo());
-
-        System.out.println("\n--- RENDIMENTO ---");
-        conta.aplicarRendimento();
-        System.out.println("Saldo após rendimento: R$ "
-                + conta.getSaldo());
-
-        System.out.println("\n--- DADOS ATUALIZADOS ---");
-        conta.mostrarDados();
+        Livro livro1 = new LivroDigital("Domcasmurro","Pedro Álvares Cabral", 1987, 333);
+        Livro livro2 = new LivroFisico("Metamorfose", "Franz Kafka", 1967, 334);
+        Usuario usuario1 = new Usuario("Júlio", 2355647);
+        Biblioteca biblioteca1 = new Biblioteca("São João", "Londrina", usuario1);
+        System.out.println(livro1);
+        System.out.println(livro2);
+        System.out.println(usuario1);
+        System.out.println(biblioteca1);
+        livro1.apresentarLivro();
+        livro2.apresentarLivro();
+        usuario1.realizarEmprestimo(livro1);
+        livro1.apresentarLivro();
+        livro2.apresentarLivro();
+        livro1.exibirMensagem();
+        livro1.exibirMensagem("Mensagem específica");
+        ((LivroDigital) livro1).baixar();
+        ((LivroFisico) livro2).folhear();
     }
 }
